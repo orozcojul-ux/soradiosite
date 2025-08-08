ff82ace5beb61072b80f94a14847ceaa669b2f5a
orozcojul-ux <orozco.jul@gmail.com>
2025-08-08 22:36:58 +0200

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://bqsqwoqeqfcpkbvhasyd.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_u1oFzBaa42YP1OIYKYddXg_ZeBnAWYQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
